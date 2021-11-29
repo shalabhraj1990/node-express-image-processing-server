ea01320d1722865a4ecff1b8c99c7721f8c90250
shalabhraj1990 <shalabhrajetc@gmail.com>
2021-11-29 13:34:59 +0530

    module-4 changes

--- api/src/router.js
+++ api/src/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
 const imageProcessor = require("./imageProcessor");
+
 const photoPath = path.resolve(__dirname, "../../client/photo-viewer.html");
 
 const router = Router();
@@ -29,12 +30,17 @@ const upload = multer({
   storage,
 });
 
-router.post("/upload", upload.single("photo"), (request, response) => {
+router.post("/upload", upload.single("photo"), async (request, response) => {
   if (request.fileValidationError)
     return response.status(400).json({ error: request.fileValidationError });
 
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {}
+
   return response.status(201).json({ success: true });
 });
+
 router.get("/photo-viewer", (request, response) => {
   response.sendFile(photoPath);
 });
